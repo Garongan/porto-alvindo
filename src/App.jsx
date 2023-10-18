0fb91e3939a69cd8263682d2affc1bcee9f5a1e2
Garongan <alvindo56@gmail.com>
2023-10-19 01:27:39 +0700

    update test web api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 import BreakSection from "./components/BreakSection";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Overview from "./components/Overview";
 import Portfolio from "./components/Portfolio";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Mengirim permintaan ke Web App Google Apps Script
+    axios.get("https://script.google.com/macros/s/AKfycbz3coSEKmTRtoyjMkVKM9ujoBoEa7bGHZ2Qr559fpO81U3Wx4uQW7Bl6psIAxQAGYPN/exec")
+      .then(response => {
+        setData(response.data); // Mengatur data ke state React
+      })
+      .catch(error => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
+  console.log({ data: data });
+
   return (
     <>
       <Header />
