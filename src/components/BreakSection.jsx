934c8d9108ccb10a7b39906f67cebfc0921f160c
Alvindo Tri Jatmiko <93327526+Garongan@users.noreply.github.com>
2023-11-27 14:09:37 +0700

    Update BreakSection.jsx

--- src/components/BreakSection.jsx
+++ src/components/BreakSection.jsx
@@ -5,7 +5,7 @@ const BreakSection = () => {
     <div className="py-6 bg-slate-900">
       <div className="text-3xl font-medium py-3 flex justify-center">{`Let's Gets Work`}</div>
       <RedirectButton
-        url={"https://wa.me/+6×8999015103"}
+        url={"https://wa.me/+628999015103"}
         buttonText={"Hire me"}
       />
     </div>
