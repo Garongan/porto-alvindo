12c993557da4a742fe03444a28d5788f8e90208f
alvindo <alvindo56@gmail.com>
2024-06-07 13:51:15 +0700

    feat: marque in power component

--- src/components/Power.jsx
+++ src/components/Power.jsx
@@ -1,53 +1,51 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Marquee from "react-fast-marquee";
 
 /* eslint-disable react/prop-types */
 const Power = ({ power }) => {
-  const item = Array.from({ length: 6 }, (x, index) => index + 1);
-  const [isLoading, setIsLoading] = useState(false);
+    const item = Array.from({ length: 6 }, (x, index) => index + 1);
+    const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(true);
-    }, 1000);
-  }, [isLoading]);
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(true);
+        }, 1000);
+    }, [isLoading]);
 
-  return (
-    <div className="py-6">
-      <div className="text-3xl container mx-auto px-8">My Power</div>
-      <div className="bg-slate-900/50 my-6 px-4">
-        <div className="container p-4 mx-auto grid lg:grid-cols-6 sm:grid-cols-2 grid-cols-1 gap-4">
-          {isLoading ? (
-            <>
-              {power.map((item, index) => {
-                return (
-                  <div
-                    className="bg-slate-950 uppercase text-md rounded-lg p-3 hover:underline shadow-lg transition-shadow hover:shadow-lg-dark"
-                    key={index}
-                  >
-                    {item}
-                  </div>
-                );
-              })}
-            </>
-          ) : (
-            <>
-              {item.map((index) => {
-                return (
-                  <div
-                    className="animate-pulse bg-slate-950 uppercase text-md rounded-lg p-3 hover:underline shadow-lg transition-shadow hover:shadow-lg-dark"
-                    key={index}
-                  >
-                    power
-                  </div>
-                );
-              })}
-            </>
-          )}
+    return (
+        <div className="py-6">
+            <div className="text-3xl container mx-auto px-8">My Power</div>
+            <div className="bg-slate-900/50 my-6">
+                {isLoading ? (
+                    <Marquee pauseOnHover gradient gradientColor="#020617" autoFill>
+                        {power.map((item, index) => {
+                            return (
+                                <div
+                                    className="bg-slate-950 m-4 uppercase text-md rounded-lg p-3 hover:underline shadow-lg transition-shadow hover:shadow-lg-dark"
+                                    key={index}
+                                >
+                                    {item}
+                                </div>
+                            );
+                        })}
+                    </Marquee>
+                ) : (
+                    <div className="container p-4 mx-auto grid lg:grid-cols-6 sm:grid-cols-2 grid-cols-1 gap-4">
+                        {item.map((index) => {
+                            return (
+                                <div
+                                    className="animate-pulse bg-slate-950 uppercase text-md rounded-lg p-3 hover:underline shadow-lg transition-shadow hover:shadow-lg-dark"
+                                    key={index}
+                                >
+                                    power
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Power;
--- src/components/SampleImg.jsx
+++ src/components/SampleImg.jsx
@@ -16,11 +16,11 @@ const SampleImg = ({ src, name, aspect }) => {
         onLoad={handleLoad}
         className={`${
           isLoading ? "hidden" : "block"
-        } ${aspect} h-[8rem] sm:h-48 lg:h-80`}
+        } ${aspect} h-[8rem] sm:h-48 md:h-64 xl:h-96`}
         loading="eager"
       />
       <img
-        className={`animate-pulse ${aspect} h-[8rem] sm:h-48 lg:h-80 bg-slate-950 rounded-lg fadeOut border-2 ${
+        className={`animate-pulse ${aspect} h-[8rem] sm:h-48 md:h-64 xl:h-96 bg-slate-950 rounded-lg fadeOut border-2 ${
           isLoading ? "block" : "hidden"
         }`}
       />
